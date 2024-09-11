af32a3ff559e5e72ea497bee1f4a405ffdcf2c31
Moaz-Elleithy <elleithy.moaz@gmail.com>
2024-09-11 13:21:11 +0300

    fixing a merge error

--- PokerBlitz/Assets/Scripts/Card.cs
+++ PokerBlitz/Assets/Scripts/Card.cs
@@ -6,27 +6,27 @@ public class Card
 {
     public enum Denomination
     {
-        Two = 2,
-        Three = 3,
-        Four = 4,
-        Five = 5,
-        Six = 6,
-        Seven = 7,
-        Eight = 8,
-        Nine = 9,
-        Ten = 10,
-        Jack = 11,
-        Queen = 12,
-        King = 13,
-        Ace = 14
+        Two,
+        Three,
+        Four,
+        Five,
+        Six ,
+        Seve,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
     }
 
     public enum Suit
     {
-        Spades = 1,
-        Diamonds = 2,
-        Clubs = 3,
-        Hearts = 4
+        Spades,
+        Diamonds,
+        Clubs,
+        Hearts
     }
 
     private Denomination denomination;
